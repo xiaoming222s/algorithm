9eed7f3702be5e6efc53e29aa954fc77313dee7e
liming <gw00217350.123>
2021-03-04 14:26:14 +0800

    排序算法

--- src/main/java/sorts/Sorts.java
+++ src/main/java/sorts/Sorts.java
@@ -4,18 +4,20 @@ import java.util.Arrays;
 
 /**
  * 冒泡排序、插入排序、选择排序
- * <p>
- * Author: Zheng
+ * <p>             交换次数
+ * 5 4 3 2 1          4
+ * 4 3 2 1 5          3
  */
 public class Sorts {
 
-    // 冒泡排序，a是数组，n表示数组大小
+    // 冒泡排序，a是数组，n表示数组大小 （从小到大排序）
     public static void bubbleSort(int[] a, int n) {
         if (n <= 1) return;
 
         for (int i = 0; i < n; ++i) {
             // 提前退出标志位
             boolean flag = false;
+            //交换次数
             for (int j = 0; j < n - i - 1; ++j) {
                 if (a[j] > a[j + 1]) { // 交换
                     int tmp = a[j];
@@ -32,7 +34,7 @@ public class Sorts {
     /**
      * 冒泡排序改进:在每一轮排序后记录最后一次元素交换的位置,作为下次比较的边界,
      * 对于边界外的元素在下次循环中无需比较.
-     */ 
+     */
     public static void bubbleSort2(int[] a, int n) {
         if (n <= 1) return;
 
@@ -59,28 +61,47 @@ public class Sorts {
         }
     }
 
-    // 插入排序，a表示数组，n表示数组大小
-    public static void insertionSort(int[] a, int n) {
-        if (n <= 1) return;
-
+    /**
+     * 从小到大排序
+     * 插入排序，a表示数组，n表示数组大小
+     * 我们将数组中的数据分为两个区间，已排序区间和未排序区间。初始已排序区间只有一个元素，就是数组的第一个元素。
+     * 插入算法的核心思想是取未排序区间中的元素，在已排序区间中找到合适的插入位置将其插入，并保证已排序区间数据一直有序。重复这个过程，直到未排序区间中元素为空，算法结
+     * <p>
+     * 插入排序也包含两种操作，一种是元素的比较，一种是元素的移动。当我们需要将一个数据 a 插入到已排序区间时，需要拿 a 与已排序区间的元素依次比较大小，找到合适的插入位置。找到插入点之后，我们还需要将插入点之后的元素顺序往后移动一位，这样才能腾出位置给元素 a 插入。
+     */
+    public static int[] insertionSort(int[] a, int n) {
+        // n=1 代表有序空间为1 不用排
+//        if (n <= 1) return;
+        if (n <= 1) return a;
+        //从第二条数据开始   第一个数为有序
         for (int i = 1; i < n; ++i) {
             int value = a[i];
+            //有序空间的 end
             int j = i - 1;
-            // 查找要插入的位置并移动数据
+            // 移动数据 保证有已排序空间 有序
             for (; j >= 0; --j) {
+                //插入排序也包含两种操作，一种是元素的比较，一种是元素的移动。当我们需要将一个数据 a 插入到已排序区间时，需要拿 a 与已排序区间的元素依次比较大小，找到合适的插入位置
+                // 找到插入点之后，我们还需要将插入点之后的元素顺序往后移动一位，这样才能腾出位置给元素 a 插入。
                 if (a[j] > value) {
                     a[j + 1] = a[j];
                 } else {
                     break;
                 }
             }
+            //找到位置 插入数据
             a[j + 1] = value;
         }
+        return  a;
     }
 
-    // 选择排序，a表示数组，n表示数组大小
-    public static void selectionSort(int[] a, int n) {
-        if (n <= 1) return;
+    /*
+      选择排序，a表示数组，n表示数组大小
+
+      选择排序算法的实现思路有点类似插入排序，也分已排序区间和未排序区间。但是选择排序每次会从未排序区间中找到最小的元素，将其放到已排序区间的末尾。
+
+     */
+    public static int[] selectionSort(int[] a, int n) {
+        if (n <= 1) return a;
 
         for (int i = 0; i < n - 1; ++i) {
             // 查找最小值
@@ -96,11 +117,14 @@ public class Sorts {
             a[i] = a[minIndex];
             a[minIndex] = tmp;
         }
+        return  a;
     }
 
     public static void main(String[] args) {
-        int[] array = new int[]{3, 4, 2, 1, 5, 6, 7, 8};
-        bubbleSort2(array, array.length);
-        System.out.println(Arrays.toString(array));
+        int[] array = new int[]{3, 4, 2, 1, 5, 6, 7, 8,0,0};
+//        bubbleSort2(array, array.length);
+//        int[] sort = insertionSort(array, 10);
+        int[] sort = selectionSort(array, 10);
+        System.out.println(Arrays.toString(sort));
     }
 }
